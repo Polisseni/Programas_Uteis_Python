08e019e9f2f9b33d114a937782fe52a2d058844f
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-06-26 19:45:44 -0300

    Verificador de maioridade

---
+++ 4_verificador_idade.py
@@ -0,0 +1,19 @@
+'''Exercício 4 — Verificador de maioridade
+
+Nível: Iniciante +
+Conceitos: if/else, comparação
+
+Objetivo:
+Receber uma idade e informar se a pessoa é maior ou menor de idade.'''
+
+# Verificador de maioridade
+
+idade = int(input("Digite sua idade: "))
+
+
+if idade >= 18:
+    print("Você é maior de idade.")
+
+else:
+    print("Você é menor de idade.")
+    
