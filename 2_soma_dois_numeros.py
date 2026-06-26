659fc368ab0550918802f92b57a204f73bf07162
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-06-26 19:42:57 -0300

    Soma de dois números

---
+++ 2_soma_dois_numeros.py
@@ -0,0 +1,16 @@
+'''Exercício 2 — Soma de dois números
+
+Nível: Iniciante
+Conceitos: input, variáveis, conversão de tipos
+
+Objetivo:
+Criar um programa que peça dois números ao usuário e mostre a soma deles.'''
+
+# Programa que soma dois números
+
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+resultado = numero1 + numero2
+
+print("A soma é:", resultado)
