b7ba5ba8cb70267b468e0df890b9d46820c04ad3
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-07-08 18:07:45 -0300

    Conversor de temperatura Celsius - Fahrenheit

---
+++ 6_conversor_temperatura.py
@@ -0,0 +1,12 @@
+'''Nível: Iniciante
+
+Conceitos:
+
+Operações matemáticas
+Entrada e saída de dados'''
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(f"{celsius}°C equivalem a {fahrenheit:.1f}°F.")
