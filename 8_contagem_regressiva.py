50d6a2cf32e4e85fb60879fafdf26b5a534908ac
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-07-08 18:10:03 -0300

    Faz uma contagem regressiva

---
+++ 8_contagem_regressiva.py
@@ -0,0 +1,13 @@
+'''Nível: Iniciante+
+
+Conceitos:
+
+Laço for
+range()'''
+
+inicio = int(input("Digite o número inicial: "))
+
+for i in range(inicio, -1, -1):
+    print(i)
+
+print("Fim da contagem!")
