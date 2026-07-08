bd28ac642402d95913522c8851ccde65d5695634
Vitor Nogueres Polisseni <vitorpolisseni68@gmail.com>
2026-07-08 18:00:35 -0300

    Verificador de par ou ímpar

---
+++ 5_verificador_par_impar.py
@@ -0,0 +1,14 @@
+'''Nível: Iniciante
+
+Conceitos:
+
+if e else
+Operador módulo (%)'''
+
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 2 == 0:
+    print(f"{numero} é um número par.")
+else:
+    print(f"{numero} é um número ímpar.")
+    
